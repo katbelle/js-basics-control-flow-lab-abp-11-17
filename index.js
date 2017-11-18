8334da4253f206ae37465edcdff2b4a8f43e7d48
Katherine Belle Hartling <katbellehartling@gmail.com>
2017-11-18 19:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code in this file!
+function scuberGreetingForFeet () {
+  let feet;
+  if (feet <= 400){
+    feet = 'This one is on me!';
+} else if (feet > 2000){
+  feet = "I will glady take your thirty bucks."
+} else {
+  feet = "No can do.";
+}
+return feet
+}
+
+function ternaryCheckCity (city) {
+return city === 'NYC'? 'Ok, sounds good.':'No go.';
+}
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+      default:
+      return 'Bye.';
+      break;
+
+  }
+}
